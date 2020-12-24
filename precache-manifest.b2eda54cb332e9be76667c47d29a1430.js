b8bb143c921eaeef3ac1a90412ff66e9b18e7d4b
Abdulbasid Guled <guled.basid@gmail.com>
2020-12-23 23:44:34 -0500

    Updates

---
+++ precache-manifest.b2eda54cb332e9be76667c47d29a1430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ae5278b942537a4b452",
+    "url": "/portfolio-website/static/css/main.dca2f6c8.chunk.css"
+  },
+  {
+    "revision": "2ae5278b942537a4b452",
+    "url": "/portfolio-website/static/js/main.2ae5278b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio-website/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "f3fd7cd97ef3bcbcdb5f2d2df96ac619",
+    "url": "/portfolio-website/index.html"
+  }
+];
